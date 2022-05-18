e23293e1ef93cb4c4a05e705b3bff4d3b45c52fc
apinuntong <apinun.tong@gmail.com>
2022-05-18 15:11:47 +0700

    Add files via upload

---
+++ jupyter_lab.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -e
+
+password='jetson'
+
+# Record the time this script starts
+date
+
+# Get the full dir name of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Keep updating the existing sudo time stamp
+sudo -v
+while true; do sudo -n true; sleep 120; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install pip and some python dependencies
+echo "\e[104m Install pip and some python dependencies \e[0m"
+sudo apt-get update
+sudo apt install -y python3-pip python3-setuptools python3-pil python3-smbus python3-matplotlib cmake curl
+sudo -H pip3 install --upgrade pip
+
+# Install jtop
+echo "\e[100m Install jtop \e[0m"
+sudo -H pip3 install jetson-stats 
+
+# Install traitlets (master, to support the unlink() method)
+echo "\e[48;5;172m Install traitlets \e[0m"
+#sudo -H python3 -m pip install git+https://github.com/ipython/traitlets@master
+sudo -H python3 -m pip install git+https://github.com/ipython/traitlets@dead2b8cdde5913572254cf6dc70b5a6065b86f8
+
+# Install JupyterLab (lock to 2.2.6, latest as of Sept 2020)
+echo "\e[48;5;172m Install Jupyter Lab 2.2.6 \e[0m"
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install -y nodejs libffi-dev libssl1.0-dev 
+sudo -H pip3 install jupyter jupyterlab==2.2.6 --verbose
+sudo -H jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+jupyter lab --generate-config
+python3 -c "from notebook.auth.security import set_password; set_password('$password', '$HOME/.jupyter/jupyter_notebook_config.json')"
+
+
+# fix for permission error
+#sudo chown -R jetson:jetson /usr/local/share/jupyter/lab/settings/build_config.json
+
+# install version of traitlets with dlink.link() feature
+# (added after 4.3.3 and commits after the one below only support Python 3.7+) 
+#
+sudo -H python3 -m pip install git+https://github.com/ipython/traitlets@dead2b8cdde5913572254cf6dc70b5a6065b86f8
+sudo -H jupyter lab build
+
+echo "\e[42m All done! \e[0m"
+
+#record the time this script ends
+date
